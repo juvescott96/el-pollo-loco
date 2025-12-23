316fd4187b23858dadf7e0917854404c0c4998ae
Dustin Condello <dustincondello@googlemail.com>
2025-12-23 19:33:39 +0700

    Add height, width, and y properties to Character and Chicken classes

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,8 @@
 class Character extends MoveableObjects {
 
+
+    height = 250;
+    y = 180;
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,6 +1,8 @@
 class Chicken extends MoveableObjects {
 
-    y = 265;
+    y = 355;
+    height = 70;
+    width = 70;
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
