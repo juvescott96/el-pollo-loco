f7a2fceb87fd60839d04daa00dbaa13247646c49
Dustin Condello <dustincondello@googlemail.com>
2025-12-30 17:33:34 +0800

    add animate for character walking moving

--- models/moveable-objects.class.js
+++ models/moveable-objects.class.js
@@ -4,12 +4,21 @@ class MoveableObjects {
     img;
     height = 175;
     width = 100;
+    imageCache = {};
 
 
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
