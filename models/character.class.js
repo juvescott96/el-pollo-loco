895bbddab0d50108f7b25ae464255032c07cdca0
Dustin Condello <dustincondello@googlemail.com>
2026-01-04 17:24:41 +0700

    add speed property and update movement logic in Character class

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MoveableObjects {
 
     height = 250;
     y = 180;
+    speed = 5;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -23,14 +24,25 @@ class Character extends MoveableObjects {
     }
 
     animate() {
+
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 50);
 
     }
 
