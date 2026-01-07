275caacc144b169dc8a5b8c2e63f862c0e02d6cf
Dustin Condello <dustincondello@googlemail.com>
2026-01-07 10:33:58 +0700

    add more chicken and update position chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -13,7 +13,7 @@ class Chicken extends MoveableObjects {
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.x = 300 + Math.random() * 300;
+        this.x = 700 + Math.random() * 1500;
         this.speed = 0.15 + Math.random() * 0.5;
 
         this.loadImages(this.IMAGES_WALKING);
